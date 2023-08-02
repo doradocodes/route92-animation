d6b69cf652c340ca5b5eac6f7ff00f03ea902771
Dora Do <dora.thien.do@gmail.com>
2023-08-02 11:23:05 -0400

    init

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const html = document.documentElement;
+const canvas = document.getElementById("r92-animation-container");
+const context = canvas.getContext("2d");
+
+const frameCount = 570;
+const currentFrame = index => (
+  `img/animation/Web_FC${index.toString().padStart(3, '0')}.png`
+)
+
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+  }
+};
+
+const img = new Image()
+img.src = currentFrame(1);
+canvas.width=1920;
+canvas.height=1080;
+img.onload=function(){
+  context.drawImage(img, 0, 0);
+}
+
+const updateImage = index => {
+  img.src = currentFrame(index);
+  context.drawImage(img, 0, 0);
+}
+
+window.addEventListener('scroll', () => {
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop / maxScrollTop;
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.ceil(scrollFraction * frameCount)
+  );
+
+  requestAnimationFrame(() => updateImage(frameIndex + 1))
+});
+
+preloadImages()
